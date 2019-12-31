5335cb43a6395e58604e12eceba74d797e35854e
Henrik Palmlund Wahlgren <henrik@pwit.se>
2019-12-31 14:34:16 +0100

    added requirments file

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ import os
 from shutil import rmtree
 
 # Package meta-data.
-NAME = 'protolog'
+NAME = "protolog"
 DESCRIPTION = "Protcol logging tool"
-URL = 'https://github.com/pwitab/protolog'
-EMAIL = 'henrik@pwit.se'
+URL = "https://github.com/pwitab/protolog"
+EMAIL = "henrik@pwit.se"
 AUTHOR = "Henrik Palmlund Wahlgren @ Palmlund Wahlgren Innovative Technology AB"
-REQUIRES_PYTHON = '>=3.6'
-VERSION = '0.0.1'
+REQUIRES_PYTHON = ">=3.6"
+VERSION = "0.0.1"
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    'click',
-]
+REQUIRED = ["click", "colorama"]
 
 # What packages are optional?
 EXTRAS = {
@@ -28,13 +26,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -44,29 +42,28 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system(
-            '{0} setup.py sdist bdist_wheel'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
+        self.status("Pushing git tags…")
         # os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
+        os.system("git push --tags")
 
         sys.exit()
 
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 setup(
@@ -74,16 +71,12 @@ setup(
     version=VERSION,
     python_requires=REQUIRES_PYTHON,
     description=DESCRIPTION,
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    entry_points={
-        'console_scripts': [
-                'protolog = protolog:cli',
-            ],
-        },
+    packages=find_packages(exclude=("tests",)),
+    entry_points={"console_scripts": ["protolog = protolog:cli"]},
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
@@ -91,17 +84,15 @@ setup(
     zip_safe=False,
     keywords=[],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand},
 )
